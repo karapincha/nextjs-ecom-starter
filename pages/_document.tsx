9a357a265841de5d76743835d2c0c89652d6413d
Kasun Peiris <kasun@karapincha.io>
2022-08-11 17:21:46 +0530

    Document update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,12 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+						rel="stylesheet"
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
